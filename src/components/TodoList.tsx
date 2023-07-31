bbc282dfef17be479e1c85776ddfc26708056ba1
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2023-07-31 09:56:51 +0200

    Use template string syntax for styled

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,12 @@
 import styled from '@emotion/styled'
 
-export const TodoList = styled.ul({
-  width: '100%',
-  listStyle: 'none',
-})
+// export const TodoList = styled.ul({
+//   width: '100%',
+//   listStyle: 'none',
+// })
+
+// Or keeping a CSS syntax:
+export const TodoList = styled.ul`
+  width: 100%;
+  list-style: square;
+`
